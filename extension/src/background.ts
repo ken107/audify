a2742b060dc668f2fab35f80967809ab43b4e900
Hai Phan <hai.phan@gmail.com>
2020-01-21 14:33:37 -0800

    ready

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,11 +1,11 @@
 (function() {
+  const audio = document.createElement("AUDIO") as HTMLAudioElement;
   chrome.runtime.onConnect.addListener(port => {
     new RpcPeer(new ExtensionMessagingPeer(port)).onInvoke = (audioUrl: string) => getAudio(audioUrl).play();
   })
 
 
   function getAudio(url: string): HTMLAudioElement {
-    const audio = document.createElement("AUDIO") as HTMLAudioElement;
     audio.src = url;
     return audio;
   }
--- website/dist/js/components.js
+++ website/dist/js/components.js
@@ -8,10 +8,44 @@ function ManageThemesPage() {
 function EditThemePage() {
 }
 
-function UploadSoundDialog() {
+function UploadSoundDialog(viewRoot) {
+    var self = this;
     var audio = document.createElement("AUDIO");
+    this.progress = 0;
+    this.error = null;
+    this.errorMessages = {
+        FILE_TOO_BIG: "Sound file too big, max 100KB",
+    }
     this.playSound = function(file) {
         audio.src = URL.createObjectURL(file);
         audio.play();
+    }
+    this.upload = function(form) {
+        if (form.soundFile.files[0].size > 100000) {
+			this.error = "FILE_TOO_BIG";
+			return;
+		}
+		this.error = null;
+		this.progress++;
+		$.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "https://support2.lsdsoftware.com/audify/upload-sound",
+            data: new FormData(form),
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function(data) {
+				self.progress--;
+				var result = data[0];
+				if (result.error) self.error = result.error;
+				else $(viewRoot).triggerHandler("success", "https://support2.lsdsoftware.com/audify/download-sound?fileId=" + result.fileId);
+            },
+            error: function(e) {
+				self.progress--;
+				self.error = (e.responseText||'').substr(0, 200);
+            }
+        })
     }
 }
