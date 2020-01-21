b8b3aaf7b2021bd37ba3c38cc869edeab980b991
Hai Phan <hai.phan@gmail.com>
2020-01-21 01:59:33 -0800

    great

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,15 +1,12 @@
 (function() {
-  const audios: {[key: string]: HTMLAudioElement} = {};
   chrome.runtime.onConnect.addListener(port => {
     new RpcPeer(new ExtensionMessagingPeer(port)).onInvoke = (audioUrl: string) => getAudio(audioUrl).play();
   })
 
 
   function getAudio(url: string): HTMLAudioElement {
-    if (!audios[url]) {
-      audios[url] = document.createElement("AUDIO") as HTMLAudioElement;
-      audios[url].src = url;
-    }
-    return audios[url];
+    const audio = document.createElement("AUDIO") as HTMLAudioElement;
+    audio.src = url;
+    return audio;
   }
 })();
--- website/dist/js/components.js
+++ website/dist/js/components.js
@@ -7,3 +7,11 @@ function ManageThemesPage() {
 
 function EditThemePage() {
 }
+
+function UploadSoundDialog() {
+    var audio = document.createElement("AUDIO");
+    this.playSound = function(file) {
+        audio.src = URL.createObjectURL(file);
+        audio.play();
+    }
+}
