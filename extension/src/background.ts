c8ced3f462a1ed095d3dfc57691f5f54a29765f0
Hai Phan <hai.phan@gmail.com>
2020-01-23 20:19:46 -0800

    update

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,12 +1,19 @@
 (function() {
-  const audio = document.createElement("AUDIO") as HTMLAudioElement;
+  const audios: {[url: string]: HTMLAudioElement} = {};
   chrome.runtime.onConnect.addListener(port => {
-    new RpcPeer(new ExtensionMessagingPeer(port)).onInvoke = (audioUrl: string) => getAudio(audioUrl).play();
+    new RpcPeer(new ExtensionMessagingPeer(port)).onInvoke = playAudio;
   })
 
 
-  function getAudio(url: string): HTMLAudioElement {
-    audio.src = url;
-    return audio;
+  function playAudio(url: string) {
+    if (audios[url]) {
+      audios[url].currentTime = 0;
+      audios[url].play();
+    }
+    else {
+      audios[url] = document.createElement("AUDIO") as HTMLAudioElement;
+      audios[url].src = url;
+      audios[url].play();
+    }
   }
 })();
